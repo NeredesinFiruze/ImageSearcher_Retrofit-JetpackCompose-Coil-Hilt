1e5541bfa23ad2e1c2f387d066c080c53e38e1b8
Mustafa <111118241+NeredesinFiruze@users.noreply.github.com>
2022-12-14 11:15:46 +0300

    Update Constants.kt

--- app/src/main/java/com/example/imagesearcherretrofit_jetpackcompose_coil_hilt/util/Constants.kt
+++ app/src/main/java/com/example/imagesearcherretrofit_jetpackcompose_coil_hilt/util/Constants.kt
@@ -2,6 +2,6 @@ package com.example.imagesearcherretrofit_jetpackcompose_coil_hilt.util
 
 object Constants {
 
-    const val KEY = "31969174-c8246c2ebedefa8889e0d5438"
+    const val KEY = "YOUR KEY HERE"
     const val BASE_URL = "https://pixabay.com/"
 }
